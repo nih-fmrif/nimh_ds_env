cd838c777801e3eca314f6a6c8efa26745999f1b
David Siebo <siebo@xango.local>
2020-05-27 15:22:53 -0700

    ProjectPaper schema

--- studious/models.py
+++ studious/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class ProjectPaper(models.Model): 
+    
+    pmcid = models.IntegerField()
+    doi = models.CharField(max_length=48)
+    journal_title = models.CharField(max_length=64)
+    title = models.CharField(max_length=250)
+    journal_year = models.IntegerField()
+    open_data = models.CharField(max_length=5)
+    data_share = models.CharField(max_length=5)
+    project_id = models.IntegerField()
+    contact_pi_project_leader = models.CharField(max_length=50)
+    organization_name = models.CharField(max_length=128)
+    
+    class Meta:
+    ordering = ('-pmcid',)
+    
+    def __str__(self): 
+        return self.title
