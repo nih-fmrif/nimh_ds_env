5e073689123e4f9432648fb36cabee8402c19ce7
David Siebo <zentraal@gmail.com>
2020-06-05 06:10:30 -0700

    ORM methods for distinct Journals and PIs

--- studious/views.py
+++ studious/views.py
@@ -2,8 +2,20 @@ from django.shortcuts import render
 from rest_framework import viewsets
 from .serializers import ProjectPaperSerializer
 from .models import ProjectPaper
-
+from django.db.models import Count
 
 class ProjectPaperViewSet(viewsets.ModelViewSet):
     queryset = ProjectPaper.objects.all().order_by('pmcid')
     serializer_class = ProjectPaperSerializer
+    
+    def countUniqueJournals:
+        vals = ProjectPaper.objects.values('journal_title')
+        journals = vals.distinct().order_by('journal_title')
+        countedJournals = journals.annotate(the_count=Count('journal_title'))
+        return countedJournals
+
+    def countUniquePIs:
+        vals = ProjectPaper.objects.values('contact_pi_project_leader')
+        pis = vals.distinct().order_by('contact_pi_project_leader')
+        countedPIs = pis.annotate(the_count=Count('contact_pi_project_leader'))
+        return countedPIs
