5e2021ca9428ee555f1aa9ecce7ee89a46d6a979
David Siebo <zentraal@gmail.com>
2020-06-10 01:24:07 -0700

    missing comma

--- studious/urls.py
+++ studious/urls.py
@@ -10,6 +10,6 @@ router.register(r'projectpapers', views.ProjectPaperViewSet)
 # Additionally, we include login URLs for the browsable API.
 urlpatterns = [
     path('', include(router.urls)),
-    path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
+    path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
     path('uniquejournals/', views.unique_journals, name='unique-journals'),
 ]
