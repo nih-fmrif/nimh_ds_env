27e14d28be014900f3623d8bdc601dde49d5a2e6
David Siebo <zentraal@gmail.com>
2020-06-09 18:17:26 -0700

    fix syntax of method declarations, missing parenthesis

--- studious/views.py
+++ studious/views.py
@@ -8,13 +8,13 @@ class ProjectPaperViewSet(viewsets.ModelViewSet):
     queryset = ProjectPaper.objects.all().order_by('pmcid')
     serializer_class = ProjectPaperSerializer
     
-    def countUniqueJournals:
+    def countUniqueJournals():
         vals = ProjectPaper.objects.values('journal_title')
         journals = vals.distinct().order_by('journal_title')
         countedJournals = journals.annotate(journal_count=Count('journal_title'))
         return countedJournals
 
-    def countUniquePIs:
+    def countUniquePIs():
         vals = ProjectPaper.objects.values('contact_pi_project_leader')
         pis = vals.distinct().order_by('contact_pi_project_leader')
         countedPIs = pis.annotate(pi_count=Count('contact_pi_project_leader'))
