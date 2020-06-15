8a8cd5a6a801c99de8091ada39022e7163c20a7e
David Siebo <zentraal@gmail.com>
2020-06-15 16:21:11 -0700

    grab needed import

--- studious/views.py
+++ studious/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from rest_framework import viewsets
 from rest_framework.decorators import api_view
 from rest_framework.decorators import permission_classes
-from rest_framework.permissions import IsAuthenticated
+from rest_framework.permissions import AllowAny
 from rest_framework.response import Response
 from .serializers import ProjectPaperSerializer
 from .serializers import UniqueJournalSerializer
