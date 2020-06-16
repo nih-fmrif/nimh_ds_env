db9f8f279c20d57ee69e0c7a0ccd113de4432b10
David Siebo <zentraal@gmail.com>
2020-06-16 05:27:30 -0700

    manage command to import orgs

---
+++ studious/management/commands/extractorgs.py
@@ -0,0 +1,15 @@
+from django.core.management.base import BaseCommand
+from studious.models import ProjectPaper
+from studious.models import Org
+
+class Command(BaseCommand):
+
+    def handle(self, *args, **kwargs):
+        vals = ProjectPaper.objects.values('organization_name')
+        orgs = vals.distinct().order_by('organization_name')
+        objs = (Org(organization_name=i["orgzniation_name"]) for i in orgs)
+
+        Org.objects.bulk_create(objs)
+        
+        print ("%s records inserted" % len(objs))
+
