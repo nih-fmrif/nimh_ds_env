a9d6c945461b677422fe2db93a29aff94a829e12
David Siebo <zentraal@gmail.com>
2020-06-16 12:07:23 -0700

    change REST batch size from 10 to 50

--- nimh_ds_env/settings.py
+++ nimh_ds_env/settings.py
@@ -134,7 +134,7 @@ REST_FRAMEWORK = {
     # or allow read-only access for unauthenticated users.
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend'],
-    'PAGE_SIZE': 10,
+    'PAGE_SIZE': 50,
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
     ]
