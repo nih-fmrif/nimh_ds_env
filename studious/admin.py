9ad180c0823236061746f166dad0ac5236a7d63e
David Siebo <zentraal@gmail.com>
2020-06-16 04:16:26 -0700

    register new types with admin

--- studious/admin.py
+++ studious/admin.py
@@ -1,5 +1,9 @@
 from django.contrib import admin
 from .models import ProjectPaper
+from .models import Org
+from .models import Person
 
 
 admin.site.register(ProjectPaper)
+admin.site.register(Org)
+admin.site.register(Person)
