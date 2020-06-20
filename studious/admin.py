073cf3c7edf83f315312a22c50c899244f44906a
David Siebo <zentraal@gmail.com>
2020-06-20 16:09:18 -0700

    register new CT with admin panel

--- studious/admin.py
+++ studious/admin.py
@@ -2,8 +2,10 @@ from django.contrib import admin
 from .models import ProjectPaper
 from .models import Org
 from .models import Person
+from .models import ArticleUpdate
 
 
 admin.site.register(ProjectPaper)
 admin.site.register(Org)
 admin.site.register(Person)
+admin.site.register(ArticleUpdate)
