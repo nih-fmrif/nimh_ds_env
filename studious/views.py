c3e134b41a2aae46c5f906fbbe6a410fff396bde
David Siebo <zentraal@gmail.com>
2020-06-21 09:50:57 +0000

    search by pmcid to confirm filtering of duplicate org-article linkages

--- studious/views.py
+++ studious/views.py
@@ -25,7 +25,7 @@ class ProjectPaperViewSet(viewsets.ModelViewSet):
     search_fields = ['id','contact_pi_project_leader','organization_name', 'pmcid']
 
 class OrgArticleViewSet(viewsets.ModelViewSet):
-    queryset = ProjectPaper.objects.all().values('pmcid','doi','journal_title','title','journal_year','open_data','data_share','organization_name').distinct().order_by('title')
+    queryset = ProjectPaper.objects.all().values('pmcid','doi','journal_title','title','journal_year','open_data','data_share','organization_name').distinct().order_by('pmcid')
     serializer_class = OrgArticleSerializer
     filter_backends = [DjangoFilterBackend]
     filterset_fields = ['org_id']
