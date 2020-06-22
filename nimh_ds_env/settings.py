6ffacf04abd7d290b3123ce0cb9ca3eabb7c2009
David Siebo <zentraal@gmail.com>
2020-06-22 09:40:38 -0700

    increate batch size so we can serve the entire unique person list in a single JSON deliver

--- nimh_ds_env/settings.py
+++ nimh_ds_env/settings.py
@@ -147,7 +147,7 @@ REST_FRAMEWORK = {
     # or allow read-only access for unauthenticated users.
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend'],
-    'PAGE_SIZE': 500,
+    'PAGE_SIZE': 5000,
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
     ]
