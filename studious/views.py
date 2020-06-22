09b322aa699cd7397803b1f9397150ed155b3856
David Siebo <zentraal@gmail.com>
2020-06-22 11:40:33 -0700

    add pmcid filter param to ProjectPaper view

--- studious/views.py
+++ studious/views.py
@@ -26,11 +26,11 @@ class ProjectPaperViewSet(viewsets.ModelViewSet):
     queryset = ProjectPaper.objects.all().order_by('pmcid')
     serializer_class = ProjectPaperSerializer
     filter_backends = [DjangoFilterBackend, filters.SearchFilter]
-    filterset_fields = ['id','contact_pi_project_leader','organization_name','pi_id','org_id']
+    filterset_fields = ['id','contact_pi_project_leader','organization_name','pi_id','org_id','pmcid']
     search_fields = ['id','contact_pi_project_leader','organization_name', 'pmcid']
 
 class OrgArticleViewSet(viewsets.ModelViewSet):
-    queryset = ProjectPaper.objects.all().values('id','pmcid','doi','journal_title','title','journal_year','open_data','data_share','organization_name').distinct().order_by('title')
+    queryset = ProjectPaper.objects.all().values('pmcid','doi','journal_title','title','journal_year','open_data','data_share','organization_name').distinct().order_by('title')
     serializer_class = OrgArticleSerializer
     filter_backends = [DjangoFilterBackend]
     filterset_fields = ['org_id']
