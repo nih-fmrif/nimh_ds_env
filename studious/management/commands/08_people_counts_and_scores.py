0bf0e523c39b86d03cb9013cfcccb58a6192cf4f
David Siebo <zentraal@gmail.com>
2020-06-21 20:34:13 -0700

    tabs to spaces for indentation syntax fix

--- studious/management/commands/08_people_counts_and_scores.py
+++ studious/management/commands/08_people_counts_and_scores.py
@@ -8,24 +8,24 @@ from studious.models import ProjectPaper
 class Command(BaseCommand):
 
     def handle(self, *args, **kwargs):
-		people = Person.objects.all()
+        people = Person.objects.all()
 
-		for p in people:
-			paper_stats = ProjectPaper.objects.filter(pi_id=p.id).aggregate(count_total_pubs=Count('pmcid'),
-																			count_data_share=Sum('data_share'),
-				                                                            count_data_open=Sum('open_data'),
-				                                                            count_data_total=Sum('data_score'),
-				                                                            data_share_score=Avg('data_share'),
-				                                                            data_open_score=Avg('open_data'),
-				                                                            data_score=Avg('data_score'),
-				                                                            )
+        for p in people:
+            paper_stats = ProjectPaper.objects.filter(pi_id=p.id).aggregate(count_total_pubs=Count('pmcid'),
+                                                                            count_data_share=Sum('data_share'),
+                                                                            count_data_open=Sum('open_data'),
+                                                                            count_data_total=Sum('data_score'),
+                                                                            data_share_score=Avg('data_share'),
+                                                                            data_open_score=Avg('open_data'),
+                                                                            data_score=Avg('data_score'),
+                                                                            )
 
-			Person.objects.filter(id=p.id).update(count_total_pubs=paper_stats['count_total_pubs'],
-				                                  count_data_share=paper_stats['count_data_share'],
-				                                  count_data_open=paper_stats['count_data_open'],
-												  count_data_total=paper_stats['count_data_total'],
-												  data_share_score=paper_stats['data_share_score'],
-												  data_open_score=paper_stats['data_open_score'],
-												  data_score=paper_stats['data_score']
-												  )
+            Person.objects.filter(id=p.id).update(count_total_pubs=paper_stats['count_total_pubs'],
+                                                  count_data_share=paper_stats['count_data_share'],
+                                                  count_data_open=paper_stats['count_data_open'],
+                                                  count_data_total=paper_stats['count_data_total'],
+                                                  data_share_score=paper_stats['data_share_score'],
+                                                  data_open_score=paper_stats['data_open_score'],
+                                                  data_score=paper_stats['data_score']
+                                                  )
 
