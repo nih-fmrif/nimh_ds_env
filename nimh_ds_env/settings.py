1b79d1e21275ed1e399dc701b17b9dfce17da92b
David Siebo <zentraal@gmail.com>
2020-06-23 06:36:02 -0700

    change REST framework to use basic auth

--- nimh_ds_env/settings.py
+++ nimh_ds_env/settings.py
@@ -148,6 +148,9 @@ REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend'],
     'PAGE_SIZE': 5000,
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework.authentication.BasicAuthentication',
+    )
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
     ]
