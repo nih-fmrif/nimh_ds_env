932f68a372b46dd2a8967ddaee8f9cf56e98d141
David Siebo <zentraal@gmail.com>
2020-06-25 18:18:54 -0700

    command to cascade ArticleUpdate changes

---
+++ studious/management/commands/cascade_article_edit.py
@@ -0,0 +1,32 @@
+from django.core.management.base import BaseCommand
+from studious.models import ProjectPaper
+from studious.models import Article
+from studious.models import ArticleUpdate
+from studious.models import Org
+from studious.models import Person
+
+class Command(BaseCommand):
+
+    def handle(self, *args, **kwargs):
+        aus = ArticleUpdate.objects.filter(is_merged=False)
+        for au in aus:
+            int_open_data = 1 if au['open_data'] == 'TRUE' else 0
+            int_data_share = 1 if au['data_share'] == 'TRUE' else 0
+            int_data_score = 1 if int_open_data==1 or int_data_share==1 else 0
+            data_score = "TRUE" if int_data_score==1 else "FALSE"
+
+            arts = Article.objects.filter(pmcid=au.pmcidid)
+            arts.update(int_open_data=int_open_data,
+                        int_data_share=int_open_data,
+                        )
+            pps = ProjectPaper.objects.filter(pmcid=au.pmcidid)
+            pps.update(open_data=au['open_data'],
+                        data_share=au['data_share'],
+                        data_score=data_score,
+                        int_open_data=int_open_data,
+                        int_data_share=int_data_share,
+                        int_data_score=int_data_score
+                        )
+
+        aus.update(is_merged=True)
+        print ("updates done")
